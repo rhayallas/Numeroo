3568e444c6737648aa2be22e481b773ea41a0d83
rhay <rhayalla.souza@aluno.educa.go.gov.br>
2025-09-09 14:41:34 -0300

    numero.js

---
+++ numero.js
@@ -0,0 +1,10 @@
+numero = [5, 10, 3, 8, 2]
+    
+for(indice = 0; indice < numero.length; indice++){
+    indice
+}
+
+soma = 0;
+for(indice = 0; indice < numero.length; indice++){
+    soma += numero[indice]
+}
